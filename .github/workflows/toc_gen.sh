1f9172935a93336fee26b801fcc13dda4ddcf3e8
Pierre Nicolas Durette <pndurette@gmail.com>
2022-12-06 00:06:39 -0800

    Limit the depth of the ToC

--- .github/workflows/toc_gen.sh
+++ .github/workflows/toc_gen.sh
@@ -5,7 +5,7 @@ sed -i '/<!-- table of contents generated below -->/q' README.md
 
 # Find directories that start with 20* (i.e year), sorted
 # e.g.: 2022, 2022/day1, 2022/day1/python, 2022/day2, 2022/day2/python, ...
-for dir in $(find 20* -maxdepth 3 -type d | sort -V); do
+for dir in $(find 20* -maxdepth 2 -type d | sort -V); do
     # Split the <dir> path into <year>/<dayn>/<lang> variables
     arrParts=(${dir//\// })
 
